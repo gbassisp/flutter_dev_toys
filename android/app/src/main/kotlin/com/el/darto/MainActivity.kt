05f690bf1a33624d8ee752c19f5ae529e9270a61
gbassisp <guilherme.bassi.sp@gmail.com>
2023-12-12 20:18:42 +1030

    init project

---
+++ android/app/src/main/kotlin/com/el/darto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.el.darto.flutter_dev_toys
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_dev_toys/app/app.dart';
+import 'package:flutter_dev_toys/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
