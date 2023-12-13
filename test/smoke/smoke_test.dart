a62bc2bb7970847d4d477245f9f6fcd25e84baba
gbassisp <guilherme.bassi.sp@gmail.com>
2023-12-13 22:47:39 +1030

    test: added smoke test of App

--- test/smoke/smoke_test.dart
+++ test/smoke/smoke_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dev_toys/app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../helpers/helpers.dart';
@@ -6,5 +7,6 @@ import '../helpers/helpers.dart';
 void main() {
   group('smoke test - pump', () {
     testWidgets('empty app', (tester) => tester.pumpApp(const SizedBox()));
+    testWidgets('app', (tester) => tester.pumpApp(const App()));
   });
 }
