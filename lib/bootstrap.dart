96bf2a2f0f1f3c8b1b816293249542f8a0a573e4
gbassisp <guilherme.bassi.sp@gmail.com>
2023-12-14 22:44:00 +1030

    feat: support more chars on password gen

--- lib/bootstrap.dart
+++ lib/bootstrap.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:developer';
 
 import 'package:flutter/widgets.dart';
+import 'package:lean_extensions/lean_extensions.dart';
 
 Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   FlutterError.onError = (details) {
@@ -9,6 +10,8 @@ Future<void> bootstrap(FutureOr<Widget> Function() builder) async {
   };
 
   // Add cross-flavor configuration here
+  LeanExtensions.charactersForRandomChar =
+      '${LeanExtensions.charactersForRandomChar}!@#%^&*()';
 
   runApp(await builder());
 }
