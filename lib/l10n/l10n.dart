8553991122d37a2a07d5c4ebeb30110c0b1ef707
gbassisp <guilherme.bassi.sp@gmail.com>
2024-01-02 21:53:07 +1030

    refactor: changed l10n output to .gen.dart

--- lib/l10n/l10n.dart
+++ lib/l10n/l10n.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_dev_toys/l10n/arb/app_localizations.dart';
-export 'package:flutter_dev_toys/l10n/arb/app_localizations.dart';
+import 'package:flutter_dev_toys/l10n/arb/app_localizations.gen.dart';
+export 'package:flutter_dev_toys/l10n/arb/app_localizations.gen.dart';
 
 extension AppLocalizationsX on BuildContext {
   AppLocalizations get l10n => AppLocalizations.of(this);
