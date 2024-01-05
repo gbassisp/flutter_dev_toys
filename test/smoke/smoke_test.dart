919cb6bee93b0c76b837b3771b226a73de4e72c1
guilherme <guilherme.bassi.sp@gmail.com>
2024-01-05 21:49:59 +1030

    test: added home screen widget test

--- test/smoke/smoke_test.dart
+++ test/smoke/smoke_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dev_toys/app/app.dart';
+import 'package:flutter_dev_toys/app/view/home.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import '../helpers/pump_app.dart';
@@ -8,5 +9,6 @@ void main() {
   group('smoke test - pump', () {
     testWidgets('empty app', (tester) => tester.pumpApp(const SizedBox()));
     testWidgets('app', (tester) => tester.pumpApp(const App()));
+    testWidgets('home', (tester) => tester.pumpApp(const HomeScreen()));
   });
 }
