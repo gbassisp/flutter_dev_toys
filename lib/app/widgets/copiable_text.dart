9b74a9aabb3f674b211268036de0b309cd72e077
guilherme <guilherme.bassi.sp@gmail.com>
2024-02-18 16:51:31 +1030

    feat: added optional label to copiable text field

--- lib/app/widgets/copiable_text.dart
+++ lib/app/widgets/copiable_text.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dev_toys/l10n/l10n.dart';
+import 'package:lean_extensions/lean_extensions.dart';
 
 class CopiableText extends StatefulWidget {
-  CopiableText({required this.text}) : super(key: UniqueKey());
+  CopiableText({required this.text, this.label}) : super(key: UniqueKey());
 
   final String text;
+  final String? label;
 
   @override
   State<CopiableText> createState() => _CopiableTextState();
@@ -25,9 +27,15 @@ class _CopiableTextState extends State<CopiableText> {
   Widget build(BuildContext context) {
     const before = Icon(Icons.copy_rounded);
     const after = Icon(Icons.check_rounded);
+    final hasLabel = !widget.label.isNullOrEmpty;
+    final title = hasLabel
+        ? Text(widget.label.orEmpty)
+        : Card(child: SelectableText(_value));
+    final subtitle = hasLabel ? Card(child: SelectableText(_value)) : null;
 
     return ListTile(
-      title: Card(child: SelectableText(_value)),
+      title: title,
+      subtitle: subtitle,
       trailing: IconButton(
         onPressed: _enabled ? _copyToClipboard : null,
         icon: _copied ? after : before,
