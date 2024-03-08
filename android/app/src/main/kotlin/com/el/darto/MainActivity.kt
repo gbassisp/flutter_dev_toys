53eb9e761299e1a9bfb661bd6faa6d5f449d7b2a
guilherme <guilherme.bassi.sp@gmail.com>
2024-03-08 21:45:07 +1030

    android reconfig

--- android/app/src/main/kotlin/com/el/darto/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.el.darto.flutter_dev_toys
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/eldarto/flutter_dev_toys/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.eldarto.flutter_dev_toys
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
